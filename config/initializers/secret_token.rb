2845a5e00048fc4ca6562f20a936b760de67e81b
JamilAbreu <abreu.jamil@gmail.com>
2011-01-29 17:18:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NDSC::Application.config.secret_token = 'b8a54980bad776c552c89d89bfd4e6a254e23b9fbfa71b70f51bdae01484cca3585720127c95b0d56f29e80b722b84afd7d4703146bb1f0ac0159bdc761e1da6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NDSC::Application.config.session_store :cookie_store, :key => '_NDSC_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NDSC::Application.config.session_store :active_record_store
