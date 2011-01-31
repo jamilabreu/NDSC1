4d0bc798b1286de05e8afc44b7ad61c797ac94d0
JamilAbreu <abreu.jamil@gmail.com>
2011-01-31 18:55:30 -0400

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def random_image
+    if params[:action] == 'index' || 'agenda'
+      @images  = ["jose.jpg", "1.jpg", "2.jpg", "3.jpg", "5.jpg"]
+      @random_no = rand(5)
+    else
+      @images  = ["jose.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg",]
+      @random_no = rand(10)
+    end
+    @random_image = @images[@random_no]
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,29 @@
 class PagesController < ApplicationController
   def index
+    random_image
   end
 
   def about
+    random_image
   end
 
   def host
+    random_image  
   end
   
   def agenda
+    random_image
   end
   
   def housing
+    random_image
   end
   
   def contact
+    random_image
   end
   
   def media
+    random_image    
   end
-
 end
