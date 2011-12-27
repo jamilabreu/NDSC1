ffbc7ae63a7d1fd6667f748e77495c1088476e4f
Jamil Abreu <abreu.jamil@gmail.com>
2011-12-27 02:53:07 -0400

    commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def eventbrite
     "http://www.eventbrite.com/event/1208349205?ref=ebtn"
   end
+  
+  def brown_signup_url
+    "https://payment.brown.edu/C20460_ustores/web/store_cat.jsp?STOREID=2&CATID=186&SINGLESTORE=true"
+  end
 end
